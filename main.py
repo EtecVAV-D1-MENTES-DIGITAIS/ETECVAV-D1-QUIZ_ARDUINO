771b71f5d0b95aeaf30aa494f9493ee18e635637
helena <helenacarrico12@gmail.com>
2025-11-05 17:22:48 -0300

    sistema de perguntas e 10 perguntas minhas

--- main.py
+++ main.py
@@ -74,5 +74,65 @@ def exibir_resultado(pontos):
     else:
         print("Não desista! Tente mais vezes!")
 
+banco_perguntas = [
+    {
+        "pergunta": "Qual é a principal função do Arduino?",
+        "alternativas": ["Controlar dispositivos eletrônicos de forma programável", "Reproduzir arquivos de áudio",
+        "Criar modelos 3D", "Servir como roteador de internet", "Desenvolver sites WEB"],
+        "correta": "Controlar dispositivos eletrônicos de forma programável"
+    },
+    {
+        "pergunta": "Qual linguagem de programação é usada no Arduino IDE?",
+        "alternativas": ["Python", "C/C++", "Java", "Ruby", "CSS"],
+        "correta": "C/C++"
+    },
+    {
+        "pergunta": "O pino digital 13 do Arduino Uno é frequentemente usado porque:",
+        "alternativas": ["Está conectado a um LED interno", "É o único que pode enviar sinal analógico", 
+        "É reservado para comunicação I2C", "Não pode ser usado como saída", "É o único que ativa movimentos"],
+        "correta": "Está conectado a um LED interno"
+    }
+    {
+        "pergunta": "O que significa “IDE” em Arduino IDE?",
+        "alternativas": ["Integrated Development Environment", "Internal Device Emulator", 
+        "Input Data Encoder", "Interactive Design Editor", "Interactive Development Encoder"],
+        "correta": "Integrated Development Environment"
+    },
+    {
+        "pergunta": "Qual é o componente responsável por processar as instruções no Arduino Uno?",
+        "alternativas": ["Sensor de temperatura", "Microcontrolador", 
+        "Resistor", "Potenciômetro", "Transistor"],
+        "correta": "Microcontrolador"
+    },
+    {
+        "pergunta": "Qual destes sensores pode ser usado com Arduino para medir distância?",
+        "alternativas": ["LDR", "Ultrassônico HC-SR04", 
+        "DHT11", "MPU6050", "DS18B20"],
+        "correta": "Ultrassônico HC-SR04"
+    },
+    {
+        "pergunta": "Qual destes componentes armazenam energia elétrica em um circuito Arduino?",
+        "alternativas": ["LED", "Resistor", "Capacitor", "Sensor", "Transistor"],
+        "correta": "Capacitor"
+    },
+    {
+        "pergunta": "A função pinMode(13, OUTPUT); serve para:",
+        "alternativas": ["Definir o pino 13 como entrada digital", "Definir o pino 13 como saída digital", 
+        "Ligar o LED no pino 13 automaticamente", "Resetar o microcontrolador", "Verificar e exibir movimento"],
+        "correta": "Definir o pino 13 como saída digital"
+    },
+    {
+        "pergunta": "O comando delay(1000); faz o Arduino:",
+        "alternativas": ["Pausar por 1 segundo", "Enviar sinal por 1 segundo", "Reiniciar o programa", 
+        "Pausar por 100 segundos", "Enviar sinal por 10 segundos"],
+        "correta": "Pausar por 1 segundo"
+    },
+    {
+        "pergunta": "Qual desses Arduinos possui conexão Wi-Fi integrada?",
+        "alternativas": ["Arduino Uno", "Arduino Nano", "Arduino Mega", "Arduino Uno Wifi Rev2", "Arduino Uno Wifi Integrated"],
+        "correta": "Arduino Uno Wifi Rev2"
+    },
+]
+
 if __name__ == "__main__":
     main()
